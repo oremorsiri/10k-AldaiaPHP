cf76a4c2abe833da2f4ac73218497caa13299451
Iris <irisromeropiquer@gmail.com>
2019-07-23 12:35:18 +0200

    menu created

--- assets/menu.php
+++ assets/menu.php
@@ -1,20 +1,22 @@
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">
-    <img src="https://geekshubs.com/wp-content/uploads/2019/02/logo-300ppp.png" height="30" alt="">
-  </a>
+<nav>
+  <div class="togglearea">
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+  <label for="toggle">
+    <span></span>
+    <span></span>
+    <span></span>
+  </label>
+
+  </div>
+
+  <input type="checkbox" id="toggle">
+
+  <div class="navbar">
+
+  <a href="#evento">Evento</a>
+  <a href="#register">Registro</a>
+  <a href="#login">Login</a>
+  <a href="#profile">Perfil</a>
+
   </div>
 </nav>
